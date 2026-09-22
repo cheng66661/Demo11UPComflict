3f7372fe7131b9f0defd7ce46d3ed96fad498a3b
cheng66661 <15720906389@163.com>
2026-09-22 22:04:13 +0800

    Add duplicate print statement in main method

--- src/main/java/com/atguigu/demo11upcomflict/Demo11UpComflictApplication.java
+++ src/main/java/com/atguigu/demo11upcomflict/Demo11UpComflictApplication.java
@@ -9,6 +9,7 @@ public class Demo11UpComflictApplication {
     public static void main(String[] args) {
         SpringApplication.run(Demo11UpComflictApplication.class, args);
         System.out.println("12");
+         System.out.println("12");
     }
 
 }
