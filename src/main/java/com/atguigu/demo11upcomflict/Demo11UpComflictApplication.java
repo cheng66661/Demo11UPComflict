c5b49a0ade6eba5a417045ea0bd273d4185ec49c
yaocheng <3492810070@qq.com>
2026-09-22 21:47:18 +0800

    first commit: Spring Boot project

---
+++ src/main/java/com/atguigu/demo11upcomflict/Demo11UpComflictApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.demo11upcomflict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo11UpComflictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo11UpComflictApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atguigu/demo11upcomflict/Demo11UpComflictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.demo11upcomflict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo11UpComflictApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
